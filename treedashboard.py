7a0655043eb4aed99d7aaf2cf4b3f3c767ed211e
Annika <59686579+annkvth@users.noreply.github.com>
2023-11-28 21:11:24 +0100

    add data file path

--- treedashboard.py
+++ treedashboard.py
@@ -2,9 +2,11 @@ import streamlit as st
 import seaborn as sns
 import pandas as pd
  
-# Load your data
-df = pd.read_csv('tree.csv')
- 
+# Load your data - read_csv should also take a zip file
+df = pd.read_csv('strassenbaumkataster_csv/strassenbaumkataster_EPSG_4326.zip')
+
+# insert the column rename parts here
+
 # Create a Seaborn correlation plot
 plot = sns.heatmap(df.corr(), annot=True)
  
