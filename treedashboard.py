7b26e6164d7f7fcb5e4fe053bc5f5a9913dd1398
Annika Vauth <a.vauth@yahoo.de>
2023-11-29 11:46:15 +0100

    add new plots with fancy colors

--- treedashboard.py
+++ treedashboard.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import numpy as np
+from adjustText import adjust_text  # Import the adjust_text function
 
 # Suppress FutureWarning messages
 import warnings
@@ -113,13 +114,94 @@ with col3:
     
     
     
-    # ### Histogram: Treetop diameter Distribution
-    # Plot a histogram of the treetop diameter
-    fig_treetop = plt.figure(figsize=(12, 8))
-    sns.distplot(df.query('kronendurchmesser_m>0')['kronendurchmesser_m'], bins=20, kde=None)
-    plt.title('Treetop diameter')
-    plt.ylabel('Number of trees')
-    plt.xlabel('Treetop diameter (m)')
-    st.pyplot(fig_treetop)
+
+fig_color = plt.figure(figsize=(12, 8))
+unique_values = df['gattung_deutsch'].unique()
+colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_values)))
+
+# Create a color dictionary
+color_dict = dict(zip(unique_values, colors))
+
+# Create a scatter plot with separate legend entries
+for gattung, color in color_dict.items():
+    subset_df = df[df['gattung_deutsch'] == gattung]
+    plt.scatter(subset_df['stammumfang_cm'], subset_df['kronendurchmesser_m'], c=color, label=gattung)
+
+# Add labels and a legend
+plt.xlabel('Stammumfang')
+plt.ylabel('Kronendurchmesser')
+plt.title('Stammumfang vs. Kronendurchmesser')
+
+# Create a legend with unique entries
+legend_entries = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=label)
+                  for label, color in color_dict.items()]
+
+plt.legend(handles=legend_entries, title='Gattung Deutsch', loc='center left', bbox_to_anchor=(1, 0.5), ncol=3)
+
+st.pyplot(fig_color)
 
 
+
+st.text('A closer look at the big trees')
+
+fig_color2 = plt.figure(figsize=(12, 8))
+## filtering condition to include species with stammumfang larger than 500 or kronendurchmesser larger than 35
+filtered_df = df[(df.groupby('gattung_deutsch')['stammumfang_cm'].transform('max') > 500) | (df['kronendurchmesser_m'] > 35)]
+
+# Get unique values and their corresponding colors
+unique_values = filtered_df['gattung_deutsch'].unique()
+colors = plt.cm.viridis(np.linspace(0, 1, len(unique_values)))
+markers = ['o', 's', '^', 'D', 'v', 'p', '*']  # Define different markers for each category
+
+#  Ensure markers array is as long as the color list
+markers *= (len(unique_values) // len(markers)) + 1
+markers = markers[:len(unique_values)]
+
+
+# Create a color dictionary
+color_dict = dict(zip(unique_values, colors))
+marker_dict = dict(zip(unique_values, markers))
+
+
+# Create a scatter plot with separate legend entries
+for gattung, color in color_dict.items():
+    subset_df = filtered_df[filtered_df['gattung_deutsch'] == gattung]
+    marker = marker_dict.get(gattung, 'o')  # Use 'o' as the default marker if gattung is not in the dictionary
+    plt.scatter(subset_df['stammumfang_cm'], subset_df['kronendurchmesser_m'], c=color, label=gattung, marker=marker)
+
+# Identify and annotate points that stand out (far to the right or high up)
+threshold_stammumfang = 600
+threshold_kronendurchmesser = 32
+highlighted_points = filtered_df[(filtered_df['stammumfang_cm'] > threshold_stammumfang) | (filtered_df['kronendurchmesser_m'] > threshold_kronendurchmesser)]
+
+
+texts = []
+for i, row in highlighted_points.iterrows():
+    text_color = color_dict.get(row['gattung_deutsch'], 'black')  # Default to black if color is not found
+    texts.append(plt.text(row['stammumfang_cm'], row['kronendurchmesser_m'], str(row['pflanzjahr']), fontsize=8, color=text_color))
+
+# Use the adjust_text function to automatically adjust the positions of the annotations to avoid overlap
+adjust_text(texts)
+
+# Add labels and a legend
+plt.xlabel('Stammumfang')
+plt.ylabel('Kronendurchmesser')
+plt.title('Stammumfang vs. Kronendurchmesser (Filtered)')
+
+# Create a legend with unique entries and move it outside the figure
+legend_entries = [plt.Line2D([0], [0], marker=marker, color='w', markerfacecolor=color, markersize=10, label=label)
+                  for label, (color, marker) in zip(color_dict.keys(), zip(colors, markers))]
+
+plt.legend(handles=legend_entries, title='Gattung Deutsch', loc='center left', bbox_to_anchor=(1, 0.5))
+st.pyplot(fig_color2)
+
+
+
+# ### Histogram: Treetop diameter Distribution
+# Plot a histogram of the treetop diameter
+fig_treetop = plt.figure(figsize=(12, 8))
+sns.distplot(df.query('kronendurchmesser_m>0')['kronendurchmesser_m'], bins=20, kde=None)
+plt.title('Treetop diameter')
+plt.ylabel('Number of trees')
+plt.xlabel('Treetop diameter (m)')
+st.pyplot(fig_treetop)
